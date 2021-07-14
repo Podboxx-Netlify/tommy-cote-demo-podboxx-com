a442fc45da1990c6a2dced74129e4a6dc4269e31
Cotetommy <nordik.tommy+1@gmail.com>
2021-07-14 18:15:52 -0400

    added podboxx to permitted domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   target: 'serverless',
   images: {
-    domains: ['podboxx-production.s3.amazonaws.com', 'podboxx-development.s3.amazonaws.com', 'next.podboxx.com', 'localhost'],
+    domains: ['podboxx-production.s3.amazonaws.com', 'podboxx-development.s3.amazonaws.com', 'next.podboxx.com', 'podboxx.com', 'localhost'],
   },
 }
