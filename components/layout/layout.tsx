76c03e0f2656d379a966a1abd512011dc2ec520d
Cotetommy <nordik.tommy+1@gmail.com>
2021-07-20 10:07:26 -0400

    ad

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -6,9 +6,25 @@ export default function Layout({...props}) {
     return (
         <div className="flex flex-col min-h-screen bg-03dp">
             <Header data={props.website}/>
+            <div className="ad left-0 invisible xl:visible ml-20"
+                 style={{
+                     backgroundImage: `url('./codeboxx-logo.png')`,
+                     backgroundSize: 'contain',
+                     // backgroundPosition: 'top',
+                     backgroundRepeat: 'no-repeat',
+                     marginTop: '390px'
+                 }}/>
             <div className='container mx-auto flex-grow'>
                 {props.children}
             </div>
+            <div className="ad-mobile visible xl:invisible h-24"
+                 style={{
+                     backgroundImage: `url('./codeboxx-logo.png')`,
+                     backgroundSize: 'contain',
+                     // backgroundAttachment: 'scroll',
+                     backgroundPosition: 'top',
+                     backgroundRepeat: 'no-repeat',
+                 }}/>
             <Footer data={props.website}/>
         </div>
     );
