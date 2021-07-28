67db111cb17179466a30969bcc619df0a04fb168
RamboWasReal <d.boutin84@gmail.com>
2021-07-28 13:11:54 -0400

    fix

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -77,7 +77,7 @@ const Post: React.FC<{ data: Data }> = ({data}) => {
                     <iframe className='h-80 w-full mb-5'
                             src={`https://player.podboxx.com/${router.query.id}?blog=true`}
                         // src={`https://player.podboxx.com/39329?blog=true`}
-                            allow="accelerometer; encrypted-media; gyroscope; picture-in-picture microphone"
+                            allow="accelerometer; encrypted-media; gyroscope; picture-in-picture; microphone"
                             allowFullScreen/>
                 </div>
                 }
